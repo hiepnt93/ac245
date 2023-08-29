eb135a7a5ab35a3b0ac72fce8ea8a4d587f5664c
hiep <hiep.nguyen@whill.inc>
2023-08-29 10:17:04 +0700

    test

--- app/etc/config.php
+++ app/etc/config.php
@@ -327,6 +327,7 @@ return [
         'Magento_WishlistGraphQl' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
-        'Temando_ShippingRemover' => 1
+        'Temando_ShippingRemover' => 1,
+        'Whill_Facility' => 1
     ]
 ];
